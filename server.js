41a75088e7b0129febfb7374436e925f1fa97aa0
Caroline Ninganga <carolninganga@gmail.com>
2020-06-04 18:41:51 -0400

    established socket.io connection

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const path = require("path");
-const express = require("express");
+const path = require('path');
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io')
+
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 //this sets the static folder to serve the public folder
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-const PORT = 3000 || process.env.PORT;
+//runs when client connects
+io.on('connection', socket => {
+    console.log('new ws connection...');
+})
 
-app.listen(PORT, () => console.log(`server running on port ${PORT}`));
+const PORT = 8080 || process.env.PORT;
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
+
