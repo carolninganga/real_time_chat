2ac18fcbec6b96822b485910d77ecd386d6bb950
Caroline Ninganga <carolninganga@gmail.com>
2020-06-04 20:22:40 -0400

    format message feature

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const http = require('http');
 const express = require('express');
-const socketio = require('socket.io')
+const socketio = require('socket.io');
+const formatMessage = require('./utils/messages');
 
 
 const app = express();
@@ -10,24 +11,24 @@ const io = socketio(server);
 
 //this sets the static folder to serve the public folder
 app.use(express.static(path.join(__dirname, 'public')));
-
+const botName = 'Chat Bot';
 //runs when client connects
 io.on('connection', socket => {
     //welcome current user
-   socket.emit('message', 'welcome to chat');
+   socket.emit('message', formatMessage(botName, 'welcome to chat'));
 
    //This line is broadcast when a new user connects
-   socket.broadcast.emit('message', 'a new user has joined the chat');
+   socket.broadcast.emit('message', formatMessage(botName, 'a new user has joined the chat'));
 
    //Runs when client disconnects
    socket.on('disconnect', () => {
-       io.emit('message', 'a user has left the chat');
+       io.emit('message', formatMessage(botName, 'a user has left the chat'));
    });
 
    //listen for chatMessage
    socket.on('chatMessage', msg => {
        console.log(msg)
-       io.emit('message', msg);
+       io.emit('message', formatMessage('User', msg));
    })
 
 });
